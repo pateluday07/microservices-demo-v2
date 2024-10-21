d2a1ff1174591b3000a068b20a9d08b1fb43290a
pateluday07 <pateluday07@gmail.com>
2024-10-21 19:45:22 +0530

    add naming server

---
+++ naming-server/src/test/java/com/pateluday07/microservicies/naming_server/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pateluday07.microservicies.naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
