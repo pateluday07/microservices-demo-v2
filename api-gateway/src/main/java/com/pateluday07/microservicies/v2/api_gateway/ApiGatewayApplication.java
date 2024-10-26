b4f78caa0f1973f4ae809521d7b166a1468c36da
pateluday07 <pateluday07@gmail.com>
2024-10-26 23:35:53 +0530

    set up api gateway

---
+++ api-gateway/src/main/java/com/pateluday07/microservicies/v2/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pateluday07.microservicies.v2.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
