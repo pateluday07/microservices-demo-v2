32868475d00156346927c5975341ac2911f6cf99
pateluday07 <pateluday07@gmail.com>
2024-10-28 01:05:10 +0530

    introduce retry and fallback mechanism

--- forex-converter-service/src/main/java/com/pateluday07/microservicies/v2/forex_converter_service/controller/CurrencyConverterController.java
+++ forex-converter-service/src/main/java/com/pateluday07/microservicies/v2/forex_converter_service/controller/CurrencyConverterController.java
@@ -1,25 +1,31 @@
 package com.pateluday07.microservicies.v2.forex_converter_service.controller;
 
-import java.math.BigDecimal;
-
 import com.pateluday07.microservicies.v2.forex_converter_service.dto.CurrencyConverterDto;
 import com.pateluday07.microservicies.v2.forex_converter_service.service.CurrencyConverterService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
+
 @RestController
 @RequestMapping("/api/currency-converter")
+@Log4j2
 @RequiredArgsConstructor
 public class CurrencyConverterController {
 
-  private final CurrencyConverterService currencyConverterService;
+    private final CurrencyConverterService currencyConverterService;
 
-  @GetMapping("/from/{from}/to/{to}/amount/{amount}")
-  public CurrencyConverterDto convertedAmount(@PathVariable String from, @PathVariable String to,
-                                              @PathVariable BigDecimal amount) {
-    return currencyConverterService.convert(from, to, amount);
-  }
+
+    @GetMapping("/from/{from}/to/{to}/amount/{amount}")
+    public CurrencyConverterDto convertedAmount(@PathVariable String from, @PathVariable String to,
+                                                @PathVariable BigDecimal amount) {
+        log.info("Converting {} To {} For The Amount {}", from, to, amount);
+        return currencyConverterService.convert(from, to, amount);
+    }
+
 }
--- forex-converter-service/src/main/java/com/pateluday07/microservicies/v2/forex_converter_service/service/CurrencyConverterServiceImpl.java
+++ forex-converter-service/src/main/java/com/pateluday07/microservicies/v2/forex_converter_service/service/CurrencyConverterServiceImpl.java
@@ -3,10 +3,14 @@ package com.pateluday07.microservicies.v2.forex_converter_service.service;
 
 import com.pateluday07.microservicies.v2.forex_converter_service.dto.CurrencyConverterDto;
 import com.pateluday07.microservicies.v2.forex_converter_service.feign.ExchangeRatesFeignService;
+import feign.FeignException;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 
@@ -15,20 +19,35 @@ import java.math.BigDecimal;
 @Log4j2
 public class CurrencyConverterServiceImpl implements CurrencyConverterService {
 
-  @Value("${server.port}")
-  private Integer port;
+    @Value("${server.port}")
+    private Integer port;
 
-  private final ExchangeRatesFeignService exchangeRatesFeignService;
+    private final ExchangeRatesFeignService exchangeRatesFeignService;
 
-  @Override
-  public CurrencyConverterDto convert(String from, String to, BigDecimal amount) {
-    log.info("Converting {} To {} For The Amount {}", from, to, amount);
-    CurrencyConverterDto converterDto = exchangeRatesFeignService.getExchangeRate(from, to);
-    log.info("Got Exchange Rate From The Forex Service {}", converterDto);
-    converterDto.setAmount(amount);
-    converterDto.setPort(port);
-    converterDto.setConvertedAmount(amount.multiply(converterDto.getRate()));
-    return converterDto;
-  }
+    @Override
+    @Retry(name = "converted-amount", fallbackMethod = "fallBackConverter")
+    public CurrencyConverterDto convert(String from, String to, BigDecimal amount) {
+        log.info("Converting {} To {} For The Amount {}", from, to, amount);
+        CurrencyConverterDto converterDto = exchangeRatesFeignService.getExchangeRate(from, to);
+        log.info("Got Exchange Rate From The Forex Service {}", converterDto);
+        converterDto.setAmount(amount);
+        converterDto.setPort(port);
+        converterDto.setConvertedAmount(amount.multiply(converterDto.getRate()));
+        return converterDto;
+    }
+
+    private CurrencyConverterDto fallBackConverter(String from, String to, BigDecimal amount, Throwable cause) {
+        if (cause instanceof FeignException feignException && HttpStatus.NOT_FOUND.value() == feignException.status()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sorry The Conversion Rate Is Not Available For "
+                    .concat(from).concat(" To ").concat(to).concat(" So We Can Not Provide The Converted Amount"), cause);
+        }
+        CurrencyConverterDto converterDto = new CurrencyConverterDto();
+        converterDto.setFrom(from);
+        converterDto.setTo(to);
+        converterDto.setAmount(amount);
+        converterDto.setPort(port);
+        converterDto.setMessage("Sorry The Forex Rates Service IS Not Available At The Moment");
+        return converterDto;
+    }
 
 }
